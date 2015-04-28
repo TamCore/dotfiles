643a2f3a5398943601b4eef232f243fecbabab6e
TamCore <philipp@tamcore.eu>
2015-04-28 21:05:23 +0200

    bin/autorandr: automatically configure the only connected output device

--- bin/autorandr.sh
+++ bin/autorandr.sh
@@ -7,13 +7,11 @@ CONF_RUN="sleep 2 && sh $HOME/.fehbg && killall -SIGUSR1 conky && killall -SIGUS
 # only LVDS1 connected
 # - deconfigure HDMI1 and VGA1
 # - auto-configure VGA1 and use as primary
-CONF_LVDS1="--output LVDS1 --auto"
 CONF_LVDS1_RUN="pacmd set-card-profile 0 output:analog-stereo+input:analog-stereo"
 
 # only HDMI1 connected
 # deconfigure VGA1 and LVDS1
 # - auto-configure HDMI1 and use as primary
-CONF_HDMI1="--output HDMI1 --auto"
 CONF_HDMI1_RUN="pacmd set-card-profile 0 output:hdmi-stereo+input:analog-stereo; amixer set Master 1+"
 
 # LVDS1 and VGA1 connected
@@ -36,24 +34,30 @@ while true
 do
   CONFIG="CONF"
   CONF_OFF=""
+  DISPLAY_COUNT=0
   while read line
   do
     output="${line%% connected*}"
     if [[ $output =~ LVDS* ]] && [ $( grep -c closed /proc/acpi/button/*/LID/state ) -eq 1 ]
     then
       echo "found ${output} but lid is closed.."
-      CONF_OFF=$output
+      CONF_OFF="--output $output --off"
     else
       echo "found ${output}.."
       CONFIG="${CONFIG}_${output}"
+      DISPLAY_COUNT=$((DISPLAY_COUNT+1))
     fi
   done < <(xrandr --current | grep ' connected')
 
   if [ "$CURRENT" != "$CONFIG" ]
   then
     CONF_OFF="$CONF_OFF $(xrandr --current | sed -n -r 's/(.*) disconnected(.*)/--output \1 --off/p' | xargs)"
+    if [ "$DISPLAY_COUNT" = "1" ]
+    then
+      declare $CONFIG="--output $output --auto"
+    fi
 
-    echo "Using $CONFIG..."
+    echo "Using $CONFIG (${!CONFIG})..."
     xrandr $CONF_OFF ${!CONFIG}
 
     run="${CONFIG}_RUN"
@@ -66,5 +70,6 @@ do
   fi
   
   echo "Sleeping $SLEEP.."
+  echo
   sleep $SLEEP
 done
